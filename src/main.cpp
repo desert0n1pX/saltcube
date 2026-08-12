077f8da7ed3e32fa395da9d695b96fc07ec3dcb6
Desert <desert0n1pX@pm.me>
2026-08-12 15:55:17 -0600

    Remove unnecessary term reset in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@ Copyright (C) 2026  @desert0n1pX <desert0n1pX ( at) pm [ dot ] me>
 */
 
 #include <exception>
-#include <iostream>
 #include <sodium.h>
 #include <stdexcept>
 #include <string>
@@ -35,7 +34,6 @@ Error codes:
 
 int main(int argc, char **argv){
     try {
-        std::cout << TERM_FORMAT_RESET ;
         options opts;
         configure_signals();
         opts.load(argc, argv);
